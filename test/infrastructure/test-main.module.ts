c6ad14dfcfc118ae0e46f7d4f043fb8a756bf9d6
Prosta4ok_ua <danylo_sydorenko@dev.abnk.uk>
2024-11-05 22:18:48 +0200

    cleanup

--- test/infrastructure/test-main.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { TestUserModule } from "./test-user.module";
-
-@Module({
-	imports: [TestUserModule],
-	exports: [TestUserModule],
-})
-export class TestMainModule {}
--- test/utils/base-module.ts
+++
@@ -1,8 +0,0 @@
-import { TestingModuleBuilder, Test } from "@nestjs/testing";
-import { AppModule } from "../../src/app.module";
-
-export const createBaseModuleFixture = (): TestingModuleBuilder => {
-	return Test.createTestingModule({
-		imports: [AppModule],
-	});
-};
